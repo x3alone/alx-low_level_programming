8047cb807be9450fa1aa8a8d9a2c8b6c6f4c4515
x3alone <otmanchouarix3@gmail.com>
2023-06-15 19:33:04 -0700

    0x00c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
- *  * main-Entry point
- *   *
- *    * Return:Always 0 (Seccess)
- *     */
+ * main-Entry point
+ *
+ * Return:Always 0 (Seccess)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
