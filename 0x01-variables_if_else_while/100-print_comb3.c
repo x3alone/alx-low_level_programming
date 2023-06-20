d9e6cda9097b61c77c7037e834174b97e8cb1c49
x3alone <otmanchouarix3@gmail.com>
2023-06-19 19:52:54 -0700

    0x01 C

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Prints all possible combinations of two different digits,
- * in ascending order, separated by a comma followed by a space.
+ *        in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
@@ -23,7 +23,7 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 			}
-		}
+		}									
 	}
 	putchar('\n');
 	return (0);
