24601c85007b7260947a27603fae95f8513ea275
x3alone <otmanchouarix3@gmail.com>
2023-06-19 19:17:27 -0700

    0x01 C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 		int n;
