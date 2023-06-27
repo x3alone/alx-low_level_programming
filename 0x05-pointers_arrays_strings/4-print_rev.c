9fe598fe47d26443be849b918a6d7987e7ac42f7
x3alone <otmanchouarix3@gmail.com>
2023-06-27 09:34:33 -0700

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,10 @@ void print_rev(char *s)
 {
 		int c;
 
-			for (c = 0; s[c] != 0; c++)
-					{
-							}
-				for (c = c - 1; c >= 0; c--)
-							_putchar(s[c]);
-					_putchar('\n');
+		for (c = 0; s[c] != 0; c++)
+		{
+		}
+		for (c = c - 1; c >= 0; c--)
+			_putchar(s[c]);
+		_putchar('\n');
 }
