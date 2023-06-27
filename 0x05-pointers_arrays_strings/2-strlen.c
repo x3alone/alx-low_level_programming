fa600b06d1c47263c2594c040a02f8c7e669c337
x3alone <otmanchouarix3@gmail.com>
2023-06-27 08:53:36 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,22 +6,12 @@
  */
 int _strlen(char *s)
 {
-	    int l = 0;
+	int l = 0;
 
-	        while (*s != '\0')
-			    {
-				            l++;
-					            s++;
-						        }
-		    return (l);
-}
-int main(void)
-{
-	    char *str;
-	        int len;
-
-		    str = "My first strlen!";
-		        len = _strlen(str);
-			    printf("%d\n", len);
-			        return (0);
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
 }
