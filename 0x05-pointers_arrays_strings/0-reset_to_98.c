dad9783acbc7c510631bb09eb89e5d7842b20c76
x3alone <otmanchouarix3@gmail.com>
2023-06-27 05:57:44 -0700

    lol

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * this fonction takes the value back to 98.
+ * pointer n returns to the number in 0-main.c
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	 
+}
